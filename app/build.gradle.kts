58d0b4839acd2e6ad49dd13b22e406da2dc642a4
Andelo <81520590+Qthefrog@users.noreply.github.com>
2024-10-30 18:40:36 -0300

    pokemon api finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,14 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Adding dependencies
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    implementation(libs.coil.compose)
+
 }
